2adfddc7ba36d047c5a83ce7b6e1fd697dcc08fb
Alex Boswell <aboswell778@gmail.com>
2021-05-03 01:03:04 -0700

    UI changes to emissary list

--- src/Emissary.js
+++ src/Emissary.js
@@ -12,7 +12,7 @@ class Emissary extends React.Component {
                 <img class="emissary" src={this.props.emissary_img}></img>
                 <div class="emissary" >
                     <h5>{this.props.emissary_name}</h5>
-                    <p class="emissary-desc">{this.props.emissary_desc}</p>
+                    {/* <p class="emissary-desc">{this.props.emissary_desc}</p> */}
                 </div>
             </div>
         );
